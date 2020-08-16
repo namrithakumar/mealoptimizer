90e53ea1c6d4dcd49b90bee5cf3cb903b48bdd45
Namritha Kumar <namrithar88@gmail.com>
2020-08-16 12:14:05 +0530

    Added a few comments

--- src/main/java/com/practice/mealoptimizer/repository/ItemRepository.java
+++ src/main/java/com/practice/mealoptimizer/repository/ItemRepository.java
@@ -11,9 +11,13 @@ import java.util.List;
  * Note: This repo will only be used when we create an admin screen for user to enter Item info.
  * Since data is loaded only from item-data.sql file, this repo will not be used for now.
  * To add a new Item, directly modify item-data.sql.
+ * Update junit test data as well by modifying item-test-data.sql.
  */
 
 public interface ItemRepository extends CrudRepository<Item, Long> {
+
+    //Used only by JUnit.
     public Item findByItemName(String itemName);
+
     public List<Item> findByItemCategoriesContains(Category category);
 }
