8898890050237d34c0b6ffdb1323d172dbb9bfff
Namritha Kumar <namrithar88@gmail.com>
2020-08-20 12:41:11 +0530

    Fixed comment

--- src/main/java/com/practice/mealoptimizer/repository/ItemRepository.java
+++ src/main/java/com/practice/mealoptimizer/repository/ItemRepository.java
@@ -9,8 +9,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 /*
- * Note: save() method in this repo will only be used when we create an admin screen for user to enter Item info.
- * Since data is loaded only from item-data.sql file, this repo will not be used for now.
+ * Note: save(), findAll() methods in this repo will only be used when we create an admin screen for user to enter Item info.
+ * Since data is loaded only from item-data.sql file, save(), findAll() will not be used for now.
  * To add a new Item, directly modify item-data.sql.
  * Update junit test data as well by modifying item-test-data.sql.
  */
