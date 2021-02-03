c145079ec2363d27dd3f9d4e8210723e03c604ab
Namritha Kumar <namrithar88@gmail.com>
2021-02-03 15:34:19 +0530

    Added spring profiles - dev and test. Fixed JUnits

--- src/main/java/com/practice/mealoptimizer/MealOptimizerApplication.java
+++ src/main/java/com/practice/mealoptimizer/MealOptimizerApplication.java
@@ -5,5 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MealOptimizerApplication {
-	public static void main(String[] args) { SpringApplication.run(MealOptimizerApplication.class, args); }
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealOptimizerApplication.class, args); }
 }
--- src/test/java/com/practice/mealoptimizer/processor/RewardOptimizerTest.java
+++ src/test/java/com/practice/mealoptimizer/processor/RewardOptimizerTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -19,6 +20,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@TestPropertySource(locations="classpath:test.properties")
 public class RewardOptimizerTest {
 
     @Autowired
